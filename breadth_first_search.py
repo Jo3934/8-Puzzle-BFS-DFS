24a03d0f8fe0237c65ed946b8299003c28d05d83
Jo3934 <114790021+Jo3934@users.noreply.github.com>
2022-10-06 16:30:47 -0700

    Update breadth_first_search.py

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,151 +1,220 @@
-# Python code to display the way from the root  
-# node to the final destination node for 8 puzzle  
-# algorithm
+""" 8 Puzzle BFS algorithmn in python """
 
-# Importing the numpy for method  
-import sys
-import numpy as np
+#import libraries 
+import numpy as np 
+import os
 
-# node  structure 
+#declare node class 
 class Node:
-	def __init__(self, state, parent, action):
-		self.state = state
-        # This will store the parent node to the  
-        # current node 
-		self.parent = parent
-		self.action = action
+    def __init__(self, node_no, data, parent, act, cost):
+        self.data = data
+        self.parent = parent
+        self.act = act
+        self.node_no = node_no
+        self.cost = cost
+#get_initial method        
+def get_initial():
+    print("Enter number between 0-9")
+    initial_state = np.zeros(9)
+    for i in range(9):
+        states = int(input("Enter " + str(i + 1) + " number: "))
+        if states < 0 or states > 8:
+            print("Enter state between [0-8]")
+            exit(0)
+        else:
+            initial_state[i] = np.array(states)
+    return np.reshape(initial_state, (3, 3))
+#method find_index
+def find_ind(puzzle):
+    i, j = np.where(puzzle == 0)
+    i = int(i)
+    j = int(j)
+    return i, j
 
+#method of movement in array left
+def move_left(data):
+    i, j = find_ind(data)
+    if j == 0:
+        return None
+    else:
+        temp_arr = np.copy(data)
+        temp = temp_arr[i, j - 1]
+        temp_arr[i, j] = temp
+        temp_arr[i, j - 1] = 0
+        return temp_arr
+#method of movement in array right   
+def move_right(data):
+    i, j = find_ind(data)
+    if j == 2:
+        return None
+    else:
+        temp_arr = np.copy(data)
+        temp = temp_arr[i, j + 1]
+        temp_arr[i, j] = temp
+        temp_arr[i, j + 1] = 0
+        return temp_arr
+#method of movement in array up
+def move_up(data):
+    i, j = find_ind(data)
+    if i == 0:
+        return None
+    else:
+        temp_arr = np.copy(data)
+        temp = temp_arr[i - 1, j]
+        temp_arr[i, j] = temp
+        temp_arr[i - 1, j] = 0
+        return temp_arr
+#method of movement in array down
+def move_down(data):
+    i, j = find_ind(data)
+    if i == 2:
+        return None
+    else:
+        temp_arr = np.copy(data)
+        temp = temp_arr[i + 1, j]
+        temp_arr[i, j] = temp
+        temp_arr[i + 1, j] = 0
+        return temp_arr
+#method of movement in array tile
+def move_tile(action, data):
+    if action == 'up':
+        return move_up(data)
+    if action == 'down':
+        return move_down(data)
+    if action == 'left':
+        return move_left(data)
+    if action == 'right':
+        return move_right(data)
+    else:
+        return None
+#method of printing states
+def print_states(list_final): 
+    print("printing final solution")
+    for l in list_final:
+        print("Move : " + str(l.act) + "\n" + "Result : " + "\n" + str(l.data) + "\t" + "node number:" + str(l.node_no))
 
-class StackFrontier:
-	def __init__(self):
-		self.frontier = []
+#method to write the details in path
+def write_path(path_formed): 
+    if os.path.exists("Path_file.txt"):
+        os.remove("Path_file.txt")
 
-	def add(self, node):
-		self.frontier.append(node)
+    f = open("Path_file.txt", "a")
+    for node in path_formed:
+        if node.parent is not None:
+            f.write(str(node.node_no) + "\t" + str(node.parent.node_no) + "\t" + str(node.cost) + "\n")
+    f.close()
+#Method defining the explored nodes
+def node_explored(explored): 
+    if os.path.exists("Node.txt"):
+        os.remove("Node.txt")
 
-	def has_state(self, state):
-		return any((node.state[0] == state[0]).all() for node in self.frontier)
-	
-	def empty(self):
-		return len(self.frontier) == 0
-	
-	def remove(self):
-		if self.empty():
-			raise Exception("Empty Stack")
-		else:
-			node = self.frontier[-1]
-			self.frontier = self.frontier[:-1]
-			return node
+    f = open("Nodes.txt", "a")
+    for element in explored:
+        f.write('[')
+        for i in range(len(element)):
+            for j in range(len(element)):
+                f.write(str(element[j][i]) + " ")
+        f.write(']')
+        f.write("\n")
+    f.close()
 
+def node_info(visited): 
+    if os.path.exists("info.txt"):
+        os.remove("info.txt")
 
-class QueueFrontier(StackFrontier):
-	def remove(self):
-		if self.empty():
-			raise Exception("Empty Queue")
-		else:
-			node = self.frontier[0]
-			self.frontier = self.frontier[1:]
-			return node
+    f = open("info.txt", "a")
+    for n in visited:
+        if n.parent is not None:
+            f.write(str(n.node_no) + "\t" + str(n.parent.node_no) + "\t" + str(n.cost) + "\n")
+    f.close()
 
+def path(node):  
+    p = []  # Empty list
+    p.append(node)
+    parent_node = node.parent
+    while parent_node is not None:
+        p.append(parent_node)
+        parent_node = parent_node.parent
+    return list(reversed(p))
 
-class PuzzleDfs:
-	def __init__(self, start, startIndex, goal, goalIndex):
-		self.start = [start, startIndex]
-		self.goal = [goal, goalIndex] 
-		self.solution = None
+def exploring_nodes(node):
+    print(" Nodes Exploration")
+    actions = ["down", "up", "left", "right"]
+    goal_node = np.array([[2, 8, 1], [0, 4, 3], [5, 6, 7]])
+    node_q = [node]
+    final_nodes = []
+    visited = []
+    final_nodes.append(node_q[0].data.tolist())  
+    #  writing data of nodes in seen
+    node_counter = 0  # To define a unique ID to all the nodes formed
+    while node_q:
+        current_root = node_q.pop(0)  # Pop the element 0 from the list
+        if current_root.data.tolist() == goal_node.tolist():
+            print("Final attained")
+            return current_root, final_nodes, visited
 
-	def neighbors(self, state):
-		mat, (row, col) = state
-		results = []
-		
-		if row > 0:
-			mat1 = np.copy(mat)
-			mat1[row][col] = mat1[row - 1][col]
-			mat1[row - 1][col] = 0
-			results.append(('up', [mat1, (row - 1, col)]))
-		if col > 0:
-			mat1 = np.copy(mat)
-			mat1[row][col] = mat1[row][col - 1]
-			mat1[row][col - 1] = 0
-			results.append(('left', [mat1, (row, col - 1)]))
-		if row < 2:
-			mat1 = np.copy(mat)
-			mat1[row][col] = mat1[row + 1][col]
-			mat1[row + 1][col] = 0
-			results.append(('down', [mat1, (row + 1, col)]))
-		if col < 2:
-			mat1 = np.copy(mat)
-			mat1[row][col] = mat1[row][col + 1]
-			mat1[row][col + 1] = 0
-			results.append(('right', [mat1, (row, col + 1)]))
+        for move in actions:
+            temp_data = move_tile(move, current_root.data)
+            if temp_data is not None:
+                node_counter += 1
+                child_node = Node(node_counter, np.array(temp_data), current_root, move, 0)  
+                # Create a child node
 
-		return results
-    
-# func to print the results
-	def print(self):
-		solution = self.solution if self.solution is not None else None
-		print("Initial:\n", self.start[0], "\n")
-		print("Final :\n",  self.goal[0], "\n")
-		print("\n Explored: ", self.num_explored, "\n")
-		for action, cell in zip(solution[0], solution[1]):
-			print("Movement: ", action, "\n", cell[0], "\n")
-		print("Final attained!!")
+                if child_node.data.tolist() not in final_nodes:  
+                    # Add the child node data in final node list
+                    node_q.append(child_node)
+                    final_nodes.append(child_node.data.tolist())
+                    visited.append(child_node)
+                    if child_node.data.tolist() == goal_node.tolist():
+                        print("Final attained")
+                        return child_node, final_nodes, visited
+    return None, None, None  
+# return statement if the goal node is not reached
 
-	def lacks_state(self, state):
-		for st in self.explored:
-			if (st[0] == state[0]).all():
-				return False
-		return True
-	
-	def solve(self):
-		self.num_explored = 0
+def check_input(l):
+    array = np.reshape(l, 9)
+    for i in range(9):
+        counter_appear = 0
+        f = array[i]
+        for j in range(9):
+            if f == array[j]:
+                counter_appear += 1
+        if counter_appear >= 2:
+            print("invalid input")
+            exit(0)
 
-		start = Node(state=self.start, parent=None, action=None)
-		frontier = QueueFrontier()
-		frontier.add(start)
+def check_solvable(g):
+    arr = np.reshape(g, 9)
+    counter_states = 0
+    for i in range(9):
+        if not arr[i] == 0:
+            check_elem = arr[i]
+            for x in range(i + 1, 9):
+                if check_elem < arr[x] or arr[x] == 0:
+                    continue
+                else:
+                    counter_states += 1
+    if counter_states % 2 == 0:
+        print("Solving 8 puzzle")
+    else:
+        print("Solving 8 puzzle failure")
 
-		self.explored = [] 
+#Main code
+k = get_initial()
 
-		while True:
-			if frontier.empty():
-				raise Exception("No Frontier")
+check_input(k)
+check_solvable(k)
 
-			node = frontier.remove()
-			self.num_explored += 1
+root = Node(0, k, None, None, 0)
 
-			if (node.state[0] == self.goal[0]).all():
-				actions = []
-				cells = []
-				while node.parent is not None:
-					actions.append(node.action)
-					cells.append(node.state)
-					node = node.parent
-				actions.reverse()
-				cells.reverse()
-				self.solution = (actions,  cells)
-				return
+# BFS implementation call
+goal, s, v = exploring_nodes(root)
 
-			self.explored.append(node.state)
-
-			for action, state in self.neighbors(node.state):
-				if not frontier.has_state(state) and self.lacks_state(state):
-					child = Node(state=state, parent=node, action=action)
-					frontier.add(child)
-
-# Main Code   
-# start form  
-# Value 0 is taken here as an empty space  
-start = np.array([[1,2,3], [8,0,4], [5, 6, 7]])
-
-# Goal form that can be solved  
-# Value 0 is an empty space  
-goal = np.array([[2, 8, 1], [0, 4, 3], [5, 6, 7]])
-
-
-startIndex = (1, 1)
-goalIndex = (1, 0)
-
-# Method call for solving the puzzle  
-s = PuzzleDfs(start, startIndex, goal, goalIndex)
-s.solve()
-s.print()
+if goal is None and s is None and v is None:
+    print("Goal State not reached")
+else:
+    # Print and write the final output
+    print_states(path(goal))
+    write_path(path(goal))
+    write_node_explored(s)
+    write_node_info(v)
